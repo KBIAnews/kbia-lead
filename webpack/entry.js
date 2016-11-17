7c4ac9a100f60681b208e2713e59616897992fe9
Nathan Lawrence <lawrencen@missouri.edu>
2016-11-17 09:45:24 -0600

    Webpack is working and it's a miracle.

---
+++ webpack/entry.js
@@ -0,0 +1,8 @@
+// Webpack entry point for KBIA Special Project The Legacy of Lead
+// Nathan Lawrence, 2016
+
+var $ = require("jquery");
+
+$(document).ready(function(){
+	console.log(window.location.pathname)
+});
